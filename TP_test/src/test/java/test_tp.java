1aec8aad065f9ce8a3a61c8b37131358b0aa74f9
BalgarokIpi <44801537+BalgarokIpi@users.noreply.github.com>
2018-11-09 16:15:04 +0100

    doit etre bon

--- TP_test/src/test/java/test_tp.java
+++ TP_test/src/test/java/test_tp.java
@@ -9,9 +9,16 @@ public class test_tp {
 		int a = 5;
 		int b = 8;
 		assertTrue(essai.modu(b,a)==3);
-		assertTrue(essai.modu(a, b)==1);
+
 		assertTrue(essai.div(b, a)==1);
+		
+	}
+	
+	@Test
+	public void testn() {
+		int a = 5;
+		int b = 8;
+		assertTrue(essai.modu(a, b)==1);
 		assertTrue(essai.div(b, a)==2);
 	}
-
 }
