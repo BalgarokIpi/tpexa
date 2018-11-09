4754250e89a1d9d74f37c3f942f64ecb01cee3e6
BalgarokIpi <44801537+BalgarokIpi@users.noreply.github.com>
2018-11-09 15:20:35 +0100

    test

---
+++ TP_test/src/main/java/essai.java
@@ -0,0 +1,12 @@
+
+public class essai {
+	
+	
+	public static int modu(int a, int b) {
+		return a%b;
+	}
+	
+	public static int div(int c, int d) {
+		return c/d;
+	}
+}
---
+++ TP_test/src/test/java/test_tp.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class test_tp {
+
+	@Test
+	public void test() {
+		int a = 5;
+		int b = 8;
+		assertTrue(essai.modu(b,a)==3);
+		assertTrue(essai.div(b, a)==1);
+	}
+
+}
