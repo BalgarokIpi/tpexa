13de41af4899e7d9f9ed71d8dff152c9e63c1138
BalgarokIpi <44801537+BalgarokIpi@users.noreply.github.com>
2018-11-09 15:22:14 +0100

    mise a jour

--- TP_test/src/test/java/test_tp.java
+++ TP_test/src/test/java/test_tp.java
@@ -9,7 +9,9 @@ public class test_tp {
 		int a = 5;
 		int b = 8;
 		assertTrue(essai.modu(b,a)==3);
+		assertTrue(essai.modu(a, b)==1);
 		assertTrue(essai.div(b, a)==1);
+		assertTrue(essai.div(b, a)==2);
 	}
 
 }
